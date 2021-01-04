ade1d0497eb13bcdc19da612f325c7a3230274ec
sonhl0723 <sonhl0723@naver.com>
2021-01-04 22:41:47 +0900

    URL id값 확인

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+
+// url이라는 모듈을 사용 할 것이다.
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryDate = url.parse(_url, true).query;
+    console.log(queryDate.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url)
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryDate.id);
  
 });
 app.listen(3001);
