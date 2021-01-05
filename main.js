69b3708ef49a110af59e47745c486a1789c8f530
sonhl0723 <sonhl0723@naver.com>
2021-01-05 18:46:37 +0900

    글 목록 수 확인

--- main.js
+++ main.js
@@ -7,38 +7,71 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryDate = url.parse(_url, true).query;
-    var title = queryDate.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryDate.id}`, 'utf8', function(err, data){
-      var description = data;
+    var pathname = url.parse(_url, true).pathname;
+    // var title = queryDate.id;
 
-      var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ul>
-    <li><a href="?id=HTML">HTML</a></li>
-    <li><a href="?id=CSS">CSS</a></li>
-    <li><a href="?id=JavaScript">JavaScript</a></li>
-  </ul>
-  <h2>${title}</h2>
-  ${description}
-</body>
-</html>
-    `;
-    response.end(template);
-    });
+    if(pathname === '/'){
+      if(queryDate.id === undefined){
+        fs.readFile(`data/${queryDate.id}`, 'utf8', function(err, data){
+          var title = 'Welcome';
+          var description = 'Hello';
+    
+          var template = `
+        <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="?id=HTML">HTML</a></li>
+        <li><a href="?id=CSS">CSS</a></li>
+        <li><a href="?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      ${description}
+    </body>
+    </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+        });
+      }
+      else{
+        fs.readFile(`data/${queryDate.id}`, 'utf8', function(err, data){
+          var title = queryDate.id;
+          var description = data;
+    
+          var template = `
+        <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="?id=HTML">HTML</a></li>
+        <li><a href="?id=CSS">CSS</a></li>
+        <li><a href="?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      ${description}
+    </body>
+    </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+        });
+      }
+      }
+      else{
+        response.writeHead(404);
+        response.end('Not found');
+      }
  
 });
 app.listen(3001);
