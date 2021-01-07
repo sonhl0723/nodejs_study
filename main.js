6fcf089bc7af8034160e2b93d08b08460978a178
sonhl0723 <sonhl0723@naver.com>
2021-01-07 18:55:04 +0900

    POST / 리다이렉션

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
 
 // url이라는 모듈을 사용 할 것이다.
 var url = require('url');
@@ -15,6 +16,7 @@ function templateHTML(title, list, body){
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href="/create">create</a>
             <h2>${title}</h2>
             ${body}
           </body>
@@ -69,11 +71,47 @@ var app = http.createServer(function(request,response){
           });
         });
       }
-      }
-      else{
-        response.writeHead(404);
-        response.end('Not found');
-      }
+    }
+    else if(pathname === '/create'){
+      fs.readdir('./data', function(err, filelist){
+        var list = templateList(filelist);
+        var title = 'WEB - CREATE';
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3001/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+                <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+          </form>
+        `);
+        
+        response.writeHead(200);
+        response.end(template);
+      })
+    }
+    else if(pathname === '/create_process'){
+        var body = '';
+
+        request.on('data', function(data){
+          body = body + data;
+        });
+        request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, `utf8`, function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          });
+        });
+    }
+    else{
+      response.writeHead(404);
+      response.end('Not found');
+    }
  
 });
 app.listen(3001);
