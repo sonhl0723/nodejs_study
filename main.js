cf3c89e82c6d36aefbb68b384d298396ef2546a7
sonhl0723 <sonhl0723@naver.com>
2021-01-07 23:40:28 +0900

    글 삭제 기능

--- main.js
+++ main.js
@@ -65,7 +65,13 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var description = data;
             var template = templateHTML(title, list, `${description}`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            `<a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>
+            <form action="/delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>
+            `);
       
             response.writeHead(200);
             response.end(template);
@@ -131,6 +137,40 @@ var app = http.createServer(function(request,response){
     
           response.writeHead(200);
           response.end(template);
+        });
+      });
+    }
+    else if(pathname === '/update_process'){
+      var body = '';
+
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}`, description, `utf8`, function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          });
+        });
+      });
+    }
+    else if(pathname === '/delete_process'){
+      var body = '';
+
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(error){
+          response.writeHead(302, {Location: `/`});
+          response.end();
         });
       });
     }
