7d0f84af3ae880d9f0db6cbcf06754fff4ad6a76
sonhl0723 <sonhl0723@naver.com>
2021-01-08 01:02:48 +0900

    모듈 활용

--- main.js
+++ main.js
@@ -1,43 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 var qs = require('querystring');
+var template = require('./lib/template.js');
 
 // url이라는 모듈을 사용 할 것이다.
 var url = require('url');
-
-function templateHTML(title, list, body, control){
-  return `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            ${control}
-            <h2>${title}</h2>
-            ${body}
-          </body>
-          </html>
-          `;
-}
-
-function templateList(filelist){
-  var list = `<ul>`;
-
-  var i = 0;
-  while(i < filelist.length){
-    list = list + `<li><a href="/?id=${filelist[i]}">
-    ${filelist[i]} </a></li>`;
-    i = i + 1;
-  }
-  
-  list = list + `</ul>`;
-
-  return list
-}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -48,23 +15,24 @@ var app = http.createServer(function(request,response){
       if(queryDate.id === undefined){
 
         fs.readdir('./data', function(err, filelist){
-          var list = templateList(filelist);
           var title = 'Welcome';
           var description = 'Hello';
-          var template = templateHTML(title, list, `${description}`,
+
+          var list = template.list(filelist);
+          var html = template.html(title, list, `${description}`,
           `<a href="/create">create</a>`);
           
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         })
       }
       else{
         fs.readdir('./data', function(err, filelist){
           fs.readFile(`data/${queryDate.id}`, 'utf8', function(err, data){
             var title = queryDate.id;
-            var list = templateList(filelist);
+            var list = template.list(filelist);
             var description = data;
-            var template = templateHTML(title, list, `${description}`,
+            var html = template.html(title, list, `${description}`,
             `<a href="/create">create</a>
             <a href="/update?id=${title}">update</a>
             <form action="/delete_process" method="post">
@@ -74,16 +42,16 @@ var app = http.createServer(function(request,response){
             `);
       
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
           });
         });
       }
     }
     else if(pathname === '/create'){
       fs.readdir('./data', function(err, filelist){
-        var list = templateList(filelist);
+        var list = template.list(filelist);
         var title = 'WEB - CREATE';
-        var template = templateHTML(title, list, `
+        var html = template.html(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -96,7 +64,7 @@ var app = http.createServer(function(request,response){
         `, '');
         
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       })
     }
     else if(pathname === '/create_process'){
@@ -117,11 +85,11 @@ var app = http.createServer(function(request,response){
     }
     else if(pathname === `/update`){
       fs.readdir('./data', function(error, filelist){
-        var list = templateList(filelist);
+        var list = template.list(filelist);
         fs.readFile(`data/${queryDate.id}`, 'utf8', function(err, data){
           var title = queryDate.id;
           var description = data;
-          var template = templateHTML(title, list, `
+          var html = template.html(title, list, `
           <form action="/update_process" method="post">
             <input type="hidden" name="id" value="${title}">
             <p><input type="text" name="title" value="${title}"></p>
@@ -136,7 +104,7 @@ var app = http.createServer(function(request,response){
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
     
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     }
@@ -169,7 +137,7 @@ var app = http.createServer(function(request,response){
         var post = qs.parse(body);
         var id = post.id;
         fs.unlink(`data/${id}`, function(error){
-          response.writeHead(302, {Location: `/`});
+          response.writeHead(302, {Location: `/` });
           response.end();
         });
       });
---
+++ nodejs/muse.js
@@ -0,0 +1,13 @@
+// var M = {
+//   v:'v',
+//   f:function(){
+//     console.log(this.v);
+//   }
+// }
+ 
+const M = require('./mpart.js');
+var part = require('./mpart.js');
+// console.log(part);
+part.f();
+
+// M.f();
